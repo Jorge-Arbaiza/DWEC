f8f701d00b0337561f9fe642692b43a63358b498
Jorge Arbaiza <aluarb894@ieselcaminas.org>
2021-02-10 16:02:33 +0100

    actualizo el plugin y el ejercicio1

--- JQuery/Práctica Examen B2/js/Ejercicio1.js
+++ JQuery/Práctica Examen B2/js/Ejercicio1.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
     $("#musica").click(function() {
-        if (($("#deporte").val('checked'))) {
-            $("#datos_php").html("");
+        if (!($("#deporte").prop('checked'))) {
+
+            $("#datos_php").html('');
         }
     })
     $("#deporte").click(function() {
-        if (($("#musica").val('checked'))) {
-            $("#div_inputs").html("");
+        if (!($("#musica").prop('checked'))) {
+            $("#div_inputs").html('');
         }
         var get_php = $.get("php/datos.php", { nombre: "Jorge", apellidos: "Arbaiza Martí" }, function(php) {
 
@@ -20,14 +21,26 @@ $(document).ready(function() {
 
     });
     $("#checkbox").click(function() {
-        $("body").append("<div id='miDiv'>Hola, aquí el texto!</div>");
-        mouse();
+        if ($("#checkbox").prop('checked')) {
+            $("body").append("<div id='miDiv'>Hola, aquí el texto!</div>");
+            mouse_in();
+            mouse_out();
+        } else {
+            $("#miDiv").remove();
+        }
     });
 
-    function mouse() {
-        $("div").mouseenter(function() {
-            $("div").css("background-color", "red");
-            $("div").css("opacity", "0.5");
+    function mouse_in() {
+        $("#miDiv").mouseover(function() {
+            $("#miDiv").css("background-color", "red");
+            $("#miDiv").css("opacity", "0.5");
+        });
+    }
+
+    function mouse_out() {
+        $("#miDiv").mouseout(function() {
+            $("#miDiv").css("background-color", "");
+            $("#miDiv").css("opacity", "1");
         });
     }
     $("h1").mouseover(function() {
--- JQuery/Práctica Examen B2/js/plugin.js
+++ JQuery/Práctica Examen B2/js/plugin.js
@@ -1,10 +1,30 @@
 jQuery.fn.plugin = function() {
     this.each(function() {
         elem = $(this);
-        elem.css("font-size", "16px");
+
         elem.animate({
             color: "blue"
         });
+        elem.queue(function() {
+
+            elem.css("font-size", "16px");
+        });
+        elem.animate({
+                opacity: "0"
+            }),
+            elem.animate({
+                opacity: "1"
+            });
+        elem.queue(function() {
+
+            elem.css("font-size", "25px");
+        });
+        elem.dequeue();
+
+
+
+
+
     });
     return this;
 };
